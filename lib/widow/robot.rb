c44bb814388aa29ce6f19a7e5c13c4371dc66e23
Dalto Curvelano Júnior <daltojr@gmail.com>
2012-04-23 08:35:06 -0300

    testing the action stack

--- lib/widow/robot.rb
+++ lib/widow/robot.rb
@@ -1,5 +1,44 @@
 module Widow
   module Robot
 
+    attr_reader :root_url
+
+    attr_reader :actions_stack
+
+    def initialize(options = {})
+      @root_url = options.delete(:root_url)
+      @client   = HTTPClient.new
+      @actions_stack = []
+    end
+
+    def get(path, options = {}, &block)
+      @actions_stack.push \
+        path: full_path(path),
+        callback: block,
+        method: :get,
+        options: options
+    end
+
+    def post(path, options = {}, &block)
+       @actions_stack.push \
+        path: full_path(path),
+        callback: block,
+        method: :post,
+        options: options
+    end
+
+    def full_path(path)
+      path.match(/^https?:\/\//) ? path : root_url + path
+    end
+
+  end
+
+  class Page
+    attr_reader :raw, :parsed
+
+    def initialize(raw)
+      @raw = raw
+      @parsed = Nokogiri::HTML(raw.dup)
+    end
   end
 end
--- spec/widow/robot_spec.rb
+++ spec/widow/robot_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
 
 describe Widow::Robot do
+  let(:robot_class) do
+    Class.new do
+      include Widow::Robot
+    end
+  end
+
+  let(:robot) do
+    robot_class.new(root_url: "http://google.com")
+  end
+
+  it "should set the root_url upon initialization" do
+    robot.root_url.should == "http://google.com"
+  end
+
+  it "should add an GET action to the actions stack" do
+    robot.actions_stack.size.should be_zero
+
+    robot.get "/" do |page|
+      1
+    end
+
+    robot.actions_stack.size.should == 1
+    robot.actions_stack.first[:method].should == :get
+
+    robot.get "/foo" do |page|
+      2
+    end
+
+    robot.actions_stack.size.should == 2
+    robot.actions_stack.last[:method].should == :get
+  end
+
+  it "should add a POST action to the action stack" do
+    robot.actions_stack.size.should be_zero
+    robot.post "/post" do
+    end
+    robot.actions_stack.size.should == 1
+    robot.actions_stack.first[:method].should == :post
+  end
+
+  it "should prepend return the url full path" do
+    robot.full_path("/").should == robot.root_url + "/"
+    robot.full_path("http://foo.bar").should == "http://foo.bar"
+  end
 
 end
+
